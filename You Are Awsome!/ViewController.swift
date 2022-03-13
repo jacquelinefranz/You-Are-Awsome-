9debc362dfef1a26f7a8d29b758bfab592046444
Jacqueline Franz <jacquelinefranz1505@gmail.com>
2022-03-13 14:52:55 +0100

    Outlet & Action Button connection

--- You Are Awsome!/ViewController.swift
+++ You Are Awsome!/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("⚡️ View Load")
     }
 
-
+    @IBAction func messageButton(_ sender: UIButton) {
+        messageLabel.text = "You Are Awsome!"
+        messageLabel.textColor = .blue
+    }
+    @IBAction func anotherMessageButton(_ sender: UIButton) {
+        messageLabel.text = "You're great!"
+        messageLabel.textColor = .red
+    }
+    
 }
 
